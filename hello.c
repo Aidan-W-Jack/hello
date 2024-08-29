ea53bb9fbd827f0372d381495b9450f7bb16e861
Aidan Jack <ajack3@huskers.unl.edu>
2024-08-29 11:29:05 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+* Author: Aidan Jack
+* Date: 2024/26/08
+*
+*A simple Aidan Jack program in C
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Aidan Jack\n");
+
+    return 0;
+}
+
